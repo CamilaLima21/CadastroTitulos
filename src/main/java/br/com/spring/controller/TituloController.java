ad6d675bc3dbb73575807c9f8b276186c74c01bf
Camila <cml.isa.17@gmail.com>
2022-06-12 21:53:05 -0300

    Mostrando a mensagem de sucesso

--- src/main/java/br/com/spring/controller/TituloController.java
+++ src/main/java/br/com/spring/controller/TituloController.java
@@ -1,14 +1,20 @@
 package br.com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.spring.model.Titulo;
+import br.com.spring.repository.TitulosRepository;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	@Autowired
+	private TitulosRepository titulosRepository;
 	
 	@RequestMapping("/novo")
 	public String novo() {
@@ -16,10 +22,12 @@ public class TituloController {
 	}
 
 	@RequestMapping(method= RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		//TODO: Salvar no banco de dados
-		System.out.println(">>> " + titulo.getDescricao());
-		return "CadastroTitulo";
+	public ModelAndView salvar(Titulo titulo) {
+		titulosRepository.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Título cadastrado com Sucesso!");
+		return mv;
 	}
+	
 	
 }
