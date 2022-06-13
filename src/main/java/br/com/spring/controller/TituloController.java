fd471ab62f82db3b786e464011b94caf82b35703
Camila <cml.isa.17@gmail.com>
2022-06-12 22:20:50 -0300

    Montando o combo dinamicamente

--- src/main/java/br/com/spring/controller/TituloController.java
+++ src/main/java/br/com/spring/controller/TituloController.java
@@ -1,11 +1,16 @@
 package br.com.spring.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.spring.model.StatusTitulo;
 import br.com.spring.model.Titulo;
 import br.com.spring.repository.TitulosRepository;
 
@@ -17,16 +22,24 @@ public class TituloController {
 	private TitulosRepository titulosRepository;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 
 	@RequestMapping(method= RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
 		titulosRepository.save(titulo);
+		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título cadastrado com Sucesso!");
 		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 	
 	
