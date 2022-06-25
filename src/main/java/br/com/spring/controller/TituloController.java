707e7b684e57f89b5c3a4802c8e7a79850e500d7
Camila <cml.isa.17@gmail.com>
2022-06-25 20:05:13 -0300

    Validação dos campos

--- src/main/java/br/com/spring/controller/TituloController.java
+++ src/main/java/br/com/spring/controller/TituloController.java
@@ -1,10 +1,15 @@
 package br.com.spring.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,16 +29,26 @@ public class TituloController {
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		mv.addObject(new Titulo());
 		return mv;
 	}
 
 	@RequestMapping(method= RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
-		titulosRepository.save(titulo);
-		
+	public ModelAndView salvar(@Valid Titulo titulo, BindingResult bindingResult) {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		if(bindingResult.hasErrors()) {
+			return mv;
+		}
+		
+		List<String> msg = new ArrayList<String>();
+		for(ObjectError objectError : bindingResult.getAllErrors()) {
+			msg.add(objectError.getDefaultMessage()); // retorna as msg das anotações 
+		}
 		mv.addObject("mensagem", "Título cadastrado com Sucesso!");
+		//mv.addObject("msg", msg);
+		titulosRepository.save(titulo);		
+		
+		
 		return mv;
 	}
 	
